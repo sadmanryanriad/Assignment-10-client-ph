f1e3ad255f5d15522ac8b2cfcf9ea0012a365427
sadmanryanriad <sadmanryanriad@gmail.com>
2023-10-20 10:45:18 +0600

    delete button linked to backend

--- src/components/Pages/Cart.jsx
+++ src/components/Pages/Cart.jsx
@@ -1,39 +1,37 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../Provider/AuthProvider";
-import CartProduct from "./pageComponents/CartProduct";
+import CartCard from "./pageComponents/CartCard";
 
 const Cart = () => {
+  const { user } = useContext(AuthContext);
+  const [products, setProducts] = useState([]);
 
-
-  const {user} = useContext(AuthContext);
-  const [products,setProducts] = useState([]);
-  
-  useEffect(()=>{
+  useEffect(() => {
     fetch(`http://localhost:3000/cart/${user?.email}`)
-    .then(res=>res.json())
-    .then(data=>{
-      console.log(data);
-      setProducts(data);
-    })
-  },[user])
-
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  }, [user]);
 
   return (
-        <div className="rounded-lg max-w-max mx-auto">
-          <h2 className="text-2xl font-bold text-center mt-5 mb-5 md:mb-10">Cart Products</h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
-          {products?.length ? (
-            products.map((card) => (
-              <CartProduct key={card._id} id={card.cart}></CartProduct>
-            ))
-          ) : (
-            <p className="text-5xl">
-              No data found.{" "}
-              <span className="text-red-500">Add product to show!</span>
-            </p>
-          )}
-          </div>
-        </div>
+    <div className="rounded-lg max-w-max mx-auto">
+      <h2 className="text-2xl font-bold text-center mt-5 mb-5 md:mb-10">
+        Cart Products
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+        {products?.length ? (
+          products.map((card) => (
+            <CartCard key={card._id} card={card}></CartCard>
+          ))
+        ) : (
+          <p className="text-5xl">
+            No data found.{" "}
+            <span className="text-red-500">Add product to show!</span>
+          </p>
+        )}
+      </div>
+    </div>
   );
 };
 
--- src/components/Pages/pageComponents/CartCard.jsx
+++ src/components/Pages/pageComponents/CartCard.jsx
@@ -1,10 +1,15 @@
 import PropTypes from "prop-types";
+import { useContext } from "react";
 import { useState } from "react";
 import { useEffect } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
 
-const CartProduct = ({ id }) => {
+const CartCard = ({ card }) => {
+  const id = card?.cart;
+  const userProductId = card?._id;
 
   const [product, setProduct] = useState({});
+  const { user } = useContext(AuthContext);
 
   useEffect(() => {
     fetch(`http://localhost:3000/brands/product/${id}`)
@@ -14,14 +19,31 @@ const CartProduct = ({ id }) => {
       });
   }, [id]);
 
+  const toDelete = () => {
+    return {
+      email: user?.email,
+      deleteId: id,
+    };
+  };
+
   const handleDelete = () => {
-    console.log("delete button clicked");
+    fetch(`http://localhost:3000/cart/${userProductId}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json(toDelete))
+      .then((data) => {
+        console.log(data);
+      });
   };
 
   return (
     <div>
       <div className="border p-4 m-4 md:m-0 rounded-lg text-center">
-        <img src={product.image} alt="" className="w-80 h-48 mx-auto rounded-lg" />
+        <img
+          src={product.image}
+          alt=""
+          className="w-80 h-48 mx-auto rounded-lg"
+        />
         <div className="p-3">
           <p className="mt-2 text-lg font-semibold">{product.name}</p>
           <p className="text-sm text-gray-500">Brand: {product.brand}</p>
@@ -44,8 +66,8 @@ const CartProduct = ({ id }) => {
   );
 };
 
-CartProduct.propTypes = {
-  id: PropTypes.string,
+CartCard.propTypes = {
+  card: PropTypes.object,
 };
 
-export default CartProduct;
+export default CartCard;
