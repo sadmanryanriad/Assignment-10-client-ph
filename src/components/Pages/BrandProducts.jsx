6e864438a5b468beeaf95290cdb59a9f1cc085ce
sadmanryanriad <sadmanryanriad@gmail.com>
2023-10-21 12:01:53 +0600

    grid updated for Cart and BrandList

--- src/components/Pages/BrandProducts.jsx
+++ src/components/Pages/BrandProducts.jsx
@@ -36,7 +36,7 @@ const BrandProducts = () => {
             <Advertisement></Advertisement>
           </div>
           <h2 className="text-4xl text-center">All Products</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {products.map((product) => (
               <ProductCard key={product._id} product={product} />
             ))}
--- src/components/Pages/Cart.jsx
+++ src/components/Pages/Cart.jsx
@@ -65,7 +65,7 @@ const Cart = () => {
           <h2 className="text-2xl font-bold text-center mt-5 mb-5 md:mb-10">
             Cart Products
           </h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
             {products?.length ? (
               products.map((card) => (
                 <CartCard
